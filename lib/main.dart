c852c51a6a7413c5f43fddfffa71c385ead7c6c4
mkmysk123456789 <mkmysk123456789@gmail.com>
2021-08-28 18:53:22 +0900

    text changed

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Admin Pannel',
+      title: 'SpicyEar',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: bgColor,
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
